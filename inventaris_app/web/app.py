1bea8221145eb1884292ef2acbdc5e71e7aaaeec
faisalrosyid <faisalrosyidbinahmed@ymail.com>
2026-06-11 18:47:39 +0700

    Add files via upload

--- inventaris_app/web/app.py
+++ inventaris_app/web/app.py
@@ -13,5 +13,5 @@ def submit():
     return f"Halo {nama}, terima kasih!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
